278c5db03dca66a14b4e3258787dddc7bc4ef1e4
HaibaneNero <nero@icvn.de>
2015-11-10 23:11:43 +0000

    fix typo

--- main.c
+++ main.c
@@ -107,7 +107,7 @@ int main(int argc, char* argv[])
 			default:
 				fprintf(stderr, "Options: \n");
 				fprintf(stderr, "\t-c n\tSet byte count per line (default 16)\n");
-				fprintf(stderr, "\t-s\tHighlight special chars and UTF-8 sequences
+				fprintf(stderr, "\t-s\tHighlight special chars and UTF-8 sequences");
 				fprintf(stderr, "\t-n\tBreak on LF / 0x0A\n");
 				return 1;
 		}
