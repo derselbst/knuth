6fc403a376786d5bfb280ebcfb828908e33b652e
Tom M <tom.mbrt@googlemail.com>
2016-02-04 14:23:32 +0100

    format

--- main.c
+++ main.c
@@ -50,183 +50,230 @@ unsigned int lineStart = 0;
 char* temp;
 char* ascBuf;
 
-void flushLine() {
-	if (currentPosition == lineStart) return;
+void flushLine()
+{
+    if (currentPosition == lineStart) return;
 
-	unsigned int bytesLeft = bpl - (currentPosition - lineStart);
+    unsigned int bytesLeft = bpl - (currentPosition - lineStart);
 
-	if (bytesLeft > 0) {
-		int spacesLeft = ((bytesLeft) * 3) - 1 + ceil((bytesLeft - 1) / 8.0);
-		while(spacesLeft--) printf(" ");
-	}
+    if (bytesLeft > 0)
+    {
+        int spacesLeft = ((bytesLeft) * 3) - 1 + ceil((bytesLeft - 1) / 8.0);
+        while(spacesLeft--) printf(" ");
+    }
 
-	printf(" | %s\n", ascBuf);
+    printf(" | %s\n", ascBuf);
 
-	ascBuf[0]='\0';
-	lineStart = currentPosition;
+    ascBuf[0]='\0';
+    lineStart = currentPosition;
 }
 
-void setStyle(char* style) {
-	sprintf(temp, "\x1B[%sm", style);
-	printf("%s", temp);
-	strcat(ascBuf, temp);
+void setStyle(char* style)
+{
+    sprintf(temp, "\x1B[%sm", style);
+    printf("%s", temp);
+    strcat(ascBuf, temp);
 }
 
-void putChars(unsigned char* data, unsigned int length, char* style) {
-	int i;
-	int style_enabled = 0;
+void putChars(unsigned char* data, unsigned int length, char* style)
+{
+    int i;
+    int style_enabled = 0;
 
-	for(i = 0; i < length; i++) {
+    for(i = 0; i < length; i++)
+    {
 
-		if (currentPosition == lineStart) {
-			if (style_enabled) setStyle("");
-			printf("%08X | ", currentPosition);
-			style_enabled = 0;
-		}
+        if (currentPosition == lineStart)
+        {
+            if (style_enabled) setStyle("");
+            printf("%08X | ", currentPosition);
+            style_enabled = 0;
+        }
 
-		if (!style_enabled && style) {
-			setStyle(style);
-			style_enabled = 1;
-		}
+        if (!style_enabled && style)
+        {
+            setStyle(style);
+            style_enabled = 1;
+        }
 
-		printf("%02X", data[i]);
+        printf("%02X", data[i]);
 
-		if (data[i] == 0) {
-			strcat(ascBuf, ".");
-		} else if (data[i] >= 0x20 && data[i] < 0x7F) {
-			sprintf(temp, "%c", data[i]);
-			strcat(ascBuf, temp);
-		} else {
-			strcat(ascBuf, ".");
-		}
+        if (data[i] == 0)
+        {
+            strcat(ascBuf, ".");
+        }
+        else if (data[i] >= 0x20 && data[i] < 0x7F)
+        {
+            sprintf(temp, "%c", data[i]);
+            strcat(ascBuf, temp);
+        }
+        else
+        {
+            strcat(ascBuf, ".");
+        }
 
-		currentPosition++;
+        currentPosition++;
 
-		if (i == length - 1 && style_enabled) {
-			setStyle("");
-		}
+        if (i == length - 1 && style_enabled)
+        {
+            setStyle("");
+        }
 
-		if ((currentPosition - lineStart) >= bpl) {
-			if (style_enabled) setStyle("");
-			flushLine();
-			style_enabled = 0;
-		} else {
-			if ((currentPosition - lineStart) % 8 == 0) {
-				printf("%s", seperator);
-			}
-			printf("%s", seperator);
-		}
+        if ((currentPosition - lineStart) >= bpl)
+        {
+            if (style_enabled) setStyle("");
+            flushLine();
+            style_enabled = 0;
+        }
+        else
+        {
+            if ((currentPosition - lineStart) % 8 == 0)
+            {
+                printf("%s", seperator);
+            }
+            printf("%s", seperator);
+        }
 
-	}
+    }
 }
 
 int main(int argc, char* argv[])
 {
-	char opt;
+    char opt;
 
-	while ((opt = getopt (argc, argv, "snc:")) != -1) {
-		switch (opt) {
-			case 's':
-				colorize = true;
-				break;
-			case 'c':
-				bpl = atoi(optarg);
-				if (bpl < 2) bpl = 2;
-				if (bpl > 1024) bpl = 1024;
-				break;
-			case 'n':
-				breakOnNl = true;
-				break;
-			default:
-                fprintf(stderr, "Syntax: %s [OPTIONS] FILE\n", argv[0]);
-                fprintf(stderr, "        %s [OPTIONS] < FILE\n", argv[0]);
-				fprintf(stderr, "Options: \n");
-				fprintf(stderr, "\t-c n\tSet byte count per line (default 16)\n");
-				fprintf(stderr, "\t-s\tHighlight special chars and UTF-8 sequences\n");
-				fprintf(stderr, "\t-n\tBreak on LF / 0x0A\n");
-				fprintf(stderr, "\n");
-				return 1;
-		}
-	}
+    while ((opt = getopt (argc, argv, "snc:")) != -1)
+    {
+        switch (opt)
+        {
+        case 's':
+            colorize = true;
+            break;
+        case 'c':
+            bpl = atoi(optarg);
+            if (bpl < 2) bpl = 2;
+            if (bpl > 1024) bpl = 1024;
+            break;
+        case 'n':
+            breakOnNl = true;
+            break;
+        default:
+            fprintf(stderr, "Syntax: %s [OPTIONS] FILE\n", argv[0]);
+            fprintf(stderr, "        %s [OPTIONS] < FILE\n", argv[0]);
+            fprintf(stderr, "Options: \n");
+            fprintf(stderr, "\t-c n\tSet byte count per line (default 16)\n");
+            fprintf(stderr, "\t-s\tHighlight special chars and UTF-8 sequences\n");
+            fprintf(stderr, "\t-n\tBreak on LF / 0x0A\n");
+            fprintf(stderr, "\n");
+            return 1;
+        }
+    }
 
-FILE* source;
-	if(!argv[optind])
+    FILE* source;
+    if(!argv[optind])
     {
         puts("No file was specified, using stdin");
         source=stdin;
     }
 
-	 source = fopen(argv[optind], "rb");
+    source = fopen(argv[optind], "rb");
 
-	temp = malloc(30);
-	ascBuf = malloc(bpl * 16);
+    temp = malloc(30);
+    ascBuf = malloc(bpl * 16);
 
-	unsigned char* c = malloc(15);
-	unsigned eatenLength = 1;
+    unsigned char* c = malloc(15);
+    unsigned eatenLength = 1;
 
-	while(fread(c, 1, 1, source) == 1) {
-		RETRY:
-		eatenLength = 1;
-		if (*c == 0) {
-			putChars(c, 1, STYLE_FAINT);
-		} else if (*c < 32 && colorize) {
-			putChars(c, 1, STYLE_BOLD";"FORE_YELLOW);
-		} else if (*c > 0x7F && colorize) {
-			int expectedLength = 0;
-			int validLength = 1;
-			char* style=BACK_BLUE;
-			if (*c < 0xC2) { // continuation or overlong
-				expectedLength = 1;
-				style = BACK_RED;
-			} else if (*c < 0xE0) {
-				expectedLength = 2;
-			} else if (*c < 0xF0) {
-				expectedLength = 3;
-			} else if (*c < 0xF8) {
-				expectedLength = 4;
-			} else if (*c < 0xFC) {
-				expectedLength = 5;
-			} else if (*c < 0xFE) {
-				expectedLength = 6;
-			} else if (*c < 0xFF) {
-				expectedLength = 7;
-			} else {
-				expectedLength = 1;
-				style = BACK_RED;
-			}
+    while(fread(c, 1, 1, source) == 1)
+    {
+RETRY:
+        eatenLength = 1;
+        if (*c == 0)
+        {
+            putChars(c, 1, STYLE_FAINT);
+        }
+        else if (*c < 32 && colorize)
+        {
+            putChars(c, 1, STYLE_BOLD";"FORE_YELLOW);
+        }
+        else if (*c > 0x7F && colorize)
+        {
+            int expectedLength = 0;
+            int validLength = 1;
+            char* style=BACK_BLUE;
+            if (*c < 0xC2)   // continuation or overlong
+            {
+                expectedLength = 1;
+                style = BACK_RED;
+            }
+            else if (*c < 0xE0)
+            {
+                expectedLength = 2;
+            }
+            else if (*c < 0xF0)
+            {
+                expectedLength = 3;
+            }
+            else if (*c < 0xF8)
+            {
+                expectedLength = 4;
+            }
+            else if (*c < 0xFC)
+            {
+                expectedLength = 5;
+            }
+            else if (*c < 0xFE)
+            {
+                expectedLength = 6;
+            }
+            else if (*c < 0xFF)
+            {
+                expectedLength = 7;
+            }
+            else
+            {
+                expectedLength = 1;
+                style = BACK_RED;
+            }
 
-			while(eatenLength < expectedLength) {
-				if (fread(c + eatenLength, 1, 1, stdin) < 1) break;
-				eatenLength++;
-				if ((*(c + eatenLength - 1) & 0xC0) == 0x80) {
-					validLength++;
-				} else {
-					break;
-				}
-			}
+            while(eatenLength < expectedLength)
+            {
+                if (fread(c + eatenLength, 1, 1, stdin) < 1) break;
+                eatenLength++;
+                if ((*(c + eatenLength - 1) & 0xC0) == 0x80)
+                {
+                    validLength++;
+                }
+                else
+                {
+                    break;
+                }
+            }
 
-			if (validLength != expectedLength) style = BACK_RED;
+            if (validLength != expectedLength) style = BACK_RED;
 
-			putChars(c, validLength, style);
+            putChars(c, validLength, style);
 
-			if (validLength != eatenLength) {
-				*c = *(c + eatenLength - 1);
-				goto RETRY;
-			}
-		} else {
-			putChars(c, 1, NULL);
-		}
+            if (validLength != eatenLength)
+            {
+                *c = *(c + eatenLength - 1);
+                goto RETRY;
+            }
+        }
+        else
+        {
+            putChars(c, 1, NULL);
+        }
 
-		if (breakOnNl && *c == 0x0A) flushLine();
+        if (breakOnNl && *c == 0x0A) flushLine();
 
-		fflush(stdout);
-	}
+        fflush(stdout);
+    }
 
-	flushLine();
+    flushLine();
 
-        free(ascBuf);
-        free(c);
-        free(temp);
+    free(ascBuf);
+    free(c);
+    free(temp);
 
-	return 0;
+    return 0;
 }
